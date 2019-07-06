239621e462070ac2356ab56fa2c345543aeeb1f5
joshzamor <josh.zamor@villagereach.org>
2019-07-06 07:46:47 -0700

    #8, prefix pcmt registry correctly
    Uses a dockerfile workaround:  https://github.com/docker/cli/issues/996#issuecomment-380424401

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 AKENEO_VER="v3.1.10"
-IMG_PREFIX=$1
+PCMT_REG=$1
 
 docker build -f pim/Dockerfile \
-    -t $IMG_PREFIX/pim \
+    -t $PCMT_REG/pim \
     --build-arg AKENEO_VER=${AKENEO_VER} \
     pim/
-docker build -f httpd/Dockerfile -t $IMG_PREFIX/httpd httpd/
+docker build -f httpd/Dockerfile -t $PCMT_REG/httpd httpd/
