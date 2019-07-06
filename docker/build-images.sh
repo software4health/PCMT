42a37c5321fcb7215ed20e22b11d3e950adc5677
joshzamor <josh.zamor@villagereach.org>
2019-07-05 22:50:22 -0700

    #8, change image names to fit gitlab registry

--- docker/build-images.sh
+++ docker/build-images.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 AKENEO_VER="v3.1.10"
+IMG_PREFIX=$1
 
 docker build -f pim/Dockerfile \
-    -t pcmt/pim \
+    -t $IMG_PREFIX/pim \
     --build-arg AKENEO_VER=${AKENEO_VER} \
     pim/
-docker build -f httpd/Dockerfile -t pcmt/httpd httpd/
+docker build -f httpd/Dockerfile -t $IMG_PREFIX/httpd httpd/
