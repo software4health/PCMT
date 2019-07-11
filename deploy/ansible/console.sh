3125cae61946695e66a65904c9770258f3116e2a
Josh Zamor <josh.zamor@villagereach.org>
2019-07-11 12:50:37 -0700

    #8, clean up and make generic helper ansible scripts

--- deploy/ansible/console.sh
+++ deploy/ansible/console.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-docker run -it --rm -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v ~/.ssh/pcmt_id_rsa:/root/.ssh/id_rsa:ro pcmt/ansible bash
+SSH_KEY_PATH=$1
+
+if [ -z "$SSH_KEY_PATH" ]; then
+    echo SSH Key path not given
+    exit 1
+fi
+
+if [ ! -r "$SSH_KEY_PATH" -o ! -f "$SSH_KEY_PATH" ]; then
+    echo SSH Key not accessible
+    exit 1
+fi
+
+docker run -it --rm \
+    -e AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY \
+    -v "$SSH_KEY_PATH:/root/.ssh/id_rsa:ro" \
+    pcmt/ansible bash
