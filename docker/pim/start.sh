1f530db204ac23f17493daf08c2a056d8757e991
Josh Zamor <josh.zamor@villagereach.org>
2019-07-15 10:23:21 -0700

    #8, fix bug where job daemon wasn't started

--- docker/pim/start.sh
+++ docker/pim/start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+function finish {
+    pkill -f job-queue-consumer-daemon
+}
+trap finish EXIT
+
 ./wait.sh mysql 3306
 ./wait.sh elasticsearch 9200
 
 bin/console --env=prod pim:install --force --symlink --clean
+bin/console --env=prod akeneo:batch:job-queue-consumer-daemon &
 
 sudo php-fpm7.2 -F
