70305b11c844cbf5aa2882179399263e4355b939
Mateusz <mateusz@mateusz_kowalewski.soldevelo>
2019-09-17 12:01:04 +0200

    #61 - set name (code) as values that show up in the dropdown list for reference data

--- pim/src/Pcmt/PcmtConnectorBundle/PcmtConnectorBundle.php
+++ pim/src/Pcmt/PcmtConnectorBundle/PcmtConnectorBundle.php
@@ -3,8 +3,16 @@ declare(strict_types=1);
 
 namespace Pcmt\PcmtConnectorBundle;
 
+use Pcmt\PcmtConnectorBundle\DependencyInjection\PcmtConnectorExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PcmtConnectorBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if(null === $this->extension){
+            $this->extension = new PcmtConnectorExtension();
+        }
+        return $this->extension;
+    }
 }
