3d75aa0aec40b07fdea3ecf374a1b15afa6a6b43
Mateusz Kowalewski <mkowalewski@soldevelo.com>
2019-11-04 06:32:15 +0000

    #127 - added concatenated attributes when fetching product model

--- pim/src/Pcmt/PcmtAttributeBundle/EventSubscriber/UpdateProductValueIfHasConcatenatedAttribute.php
+++ pim/src/Pcmt/PcmtAttributeBundle/EventSubscriber/UpdateProductValueIfHasConcatenatedAttribute.php
@@ -3,26 +3,41 @@ declare(strict_types=1);
 
 namespace Pcmt\PcmtAttributeBundle\EventSubscriber;
 
+use Akeneo\Pim\Enrichment\Component\Product\Repository\ProductModelRepositoryInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Repository\ProductRepositoryInterface;
+use Akeneo\Pim\Structure\Component\Model\FamilyVariant;
 use Akeneo\Pim\Structure\Component\Repository\AttributeRepositoryInterface;
 use Akeneo\Pim\Structure\Component\Repository\FamilyRepositoryInterface;
+use Akeneo\Pim\Structure\Component\Repository\FamilyVariantRepositoryInterface;
 use Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface;
 use Akeneo\Tool\Component\StorageUtils\Updater\ObjectUpdaterInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Pcmt\PcmtAttributeBundle\Event\ProductFetchEvent;
+use Pcmt\PcmtProductBundle\Event\ProductModelFetchEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class UpdateProductValueIfHasConcatenatedAttribute implements EventSubscriberInterface
 {
     private const IS_MISSING = 'MISSING';
     private const IS_EMPTY = 'EMPTY';
 
+    /** @var EntityManagerInterface $entityManager */
+    private $entityManager;
+
     /** @var ProductRepositoryInterface $productRepository */
     private $productRepository;
 
+    /** @var ProductModelRepositoryInterface $productModelRepository */
+    private $productModelRepository;
+
+    /** @var FamilyVariantRepositoryInterface $familyVariantRepository */
+    private $familyVariantRepository;
+
     /** @var FamilyRepositoryInterface $concatenatedAttributeRepository */
     private $concatenatedAttributeRepository;
+
+    /** @var FamilyRepositoryInterface $familyRepository */
+    private $familyRepository;
 
     /** @var AttributeRepositoryInterface $attributeRepository */
     protected $attributeRepository;
@@ -30,22 +45,40 @@ final class UpdateProductValueIfHasConcatenatedAttribute implements EventSubscri
     /** @var ObjectUpdaterInterface $productValuesUpdater */
     protected $productValuesUpdater;
 
+    /** @var ObjectUpdaterInterface $productModelValuesUpdater */
+    protected $productModelValuesUpdater;
+
     /** @var SaverInterface $productSaver */
     protected $productSaver;
 
+    /** @var SaverInterface $productModelSaver */
+    protected  $productModelSaver;
+
     public function __construct(
+        EntityManagerInterface $entityManager,
         ProductRepositoryInterface $productRepository,
+        ProductModelRepositoryInterface $productModelRepository,
+        FamilyVariantRepositoryInterface $familyVariantRepository,
         FamilyRepositoryInterface $concatenatedAttributeRepository,
+        FamilyRepositoryInterface $familyRepository,
         AttributeRepositoryInterface $attributeRepository,
         ObjectUpdaterInterface $productValuesUpdater,
-        SaverInterface $productSaver
+        ObjectUpdaterInterface $productModelValuesUpdater,
+        SaverInterface $productSaver,
+        SaverInterface $productModelSaver
     )
     {
+        $this->entityManager = $entityManager;
         $this->productRepository = $productRepository;
+        $this->productModelRepository = $productModelRepository;
+        $this->familyVariantRepository = $familyVariantRepository;
         $this->concatenatedAttributeRepository = $concatenatedAttributeRepository;
+        $this->familyRepository = $familyRepository;
         $this->attributeRepository = $attributeRepository;
         $this->productValuesUpdater = $productValuesUpdater;
+        $this->productModelValuesUpdater = $productModelValuesUpdater;
         $this->productSaver = $productSaver;
+        $this->productModelSaver = $productModelSaver;
     }
 
     public static function getSubscribedEvents(): array
@@ -53,6 +86,9 @@ final class UpdateProductValueIfHasConcatenatedAttribute implements EventSubscri
         return [
           ProductFetchEvent::class => [
               ['onProductFetch', 10]
+          ],
+          ProductModelFetchEvent::class => [
+              ['onProductModelFetch', 10]
           ]
         ];
     }
@@ -98,6 +134,52 @@ final class UpdateProductValueIfHasConcatenatedAttribute implements EventSubscri
 
             $this->productValuesUpdater->update($product, $values);
             $this->productSaver->save($product);
+        }
+    }
+
+    public function onProductModelFetch(ProductModelFetchEvent $event): void
+    {
+        $productModel = $this->productModelRepository->find(
+            $event->getProductModelId()
+        );
+
+        $familyVariant = $productModel->getFamilyVariant();
+        $concatenatedAttributes = $this->concatenatedAttributeRepository->getConcatenatedAttributes(
+            $familyVariant->getFamily()
+        );
+
+        $values = [];
+        foreach ($concatenatedAttributes as $counter => $concatenatedAttribute){
+
+            $attributeName = $concatenatedAttribute['code'];
+            $memberAttributes = $this->attributeRepository->findBy(['code' =>
+                explode(',', $concatenatedAttribute['properties']['attributes'])
+            ]);
+
+            $separator =  $concatenatedAttribute['properties']['separators'];
+            $concatenatedValue = [];
+
+            foreach ($memberAttributes as $memberAttribute){
+
+                if($productModel->hasAttribute($memberAttribute->getCode())){
+
+                    $value = $productModel->getValue($memberAttribute->getCode());
+                    if(!(null == $value || '' == $value || [] == $value)){
+                        $concatenatedValue[] = $value->__toString();
+                    }else {
+                        $concatenatedValue[] = $memberAttribute->getCode() . ' ' . self::IS_EMPTY;
+                    }
+                } else {
+                    $concatenatedValue[] = $memberAttribute->getCode() . ' ' . self::IS_MISSING;
+                }
+            }
+
+            $values[$attributeName]['data']['data'] = [implode($separator,$concatenatedValue)];
+            $values[$attributeName]['data']['locale'] = null;
+            $values[$attributeName]['data']['scope'] = null;
+
+            $this->productValuesUpdater->update($productModel, $values);
+            $this->productModelSaver->save($productModel);
         }
     }
 }
---
+++ pim/src/Pcmt/PcmtFamilyBundle/PcmtFamilyBundle.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Pcmt\PcmtFamilyBundle;
+
+use Pcmt\PcmtFamilyBundle\DependencyInjection\PcmtFamilyExtension;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PcmtFamilyBundle extends Bundle
+{
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new PcmtFamilyExtension();
+        }
+
+        return $this->extension;
+    }
+}
