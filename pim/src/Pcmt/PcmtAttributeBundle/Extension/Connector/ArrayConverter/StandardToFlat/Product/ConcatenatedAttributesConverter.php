492cada46c98b954d293547573f362a15da5703b
Mateusz <mateusz@mateusz_kowalewski.soldevelo>
2019-11-06 12:37:41 +0100

    #129 - functionality - export works

--- pim/src/Pcmt/PcmtAttributeBundle/Extension/Connector/ArrayConverter/StandardToFlat/Product/ConcatenatedAttributesConverter.php
+++ pim/src/Pcmt/PcmtAttributeBundle/Extension/Connector/ArrayConverter/StandardToFlat/Product/ConcatenatedAttributesConverter.php
@@ -3,14 +3,27 @@ declare(strict_types=1);
 
 namespace Pcmt\PcmtAttributeBundle\Extension\Connector\ArrayConverter\StandardToFlat\Product;
 
-use Akeneo\Pim\Enrichment\Component\Product\Connector\ArrayConverter\FlatToStandard\AttributeColumnsResolver;
 use Akeneo\Pim\Enrichment\Component\Product\Connector\ArrayConverter\StandardToFlat\Product\ValueConverter\AbstractValueConverter;
 use Akeneo\Pim\Enrichment\Component\Product\Connector\ArrayConverter\StandardToFlat\Product\ValueConverter\ValueConverterInterface;
 
 class ConcatenatedAttributesConverter extends AbstractValueConverter implements ValueConverterInterface
 {
-    public function convert($attributeCode, $data)
+    public function convert($attributeCode, $data): array
     {
-        return ['concatenated' => 'for now it\'s only crying' ];
+        $convertedItem = [];
+        $testoutputstring = '';
+
+        foreach ($data as $value){
+            if(is_array($value)){
+                foreach ($value as $key => $item){
+                    $testoutputstring .= $item;
+                }
+            } else {
+                $testoutputstring .= $value;
+            }
+        }
+
+        $convertedItem[$attributeCode] = $testoutputstring;
+        return $convertedItem;
     }
 }
