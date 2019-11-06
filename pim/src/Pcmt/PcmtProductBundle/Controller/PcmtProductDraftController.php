85e2d695ea25d68e1e2d92b5911421bbf9541da3
Krzysztof <kgrabowski@soldevelo.com>
2019-11-06 12:19:29 +0100

    #141 permissions connected with PcmtProductDraftController

--- pim/src/Pcmt/PcmtProductBundle/Controller/PcmtProductDraftController.php
+++ pim/src/Pcmt/PcmtProductBundle/Controller/PcmtProductDraftController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Pcmt\PcmtProductBundle\Controller;
 
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Intl\Exception\NotImplementedException;
@@ -13,7 +14,10 @@ class PcmtProductDraftController
     {
     }
 
-    /** approve existig draft */
+    /**
+     * approve existig draft
+     * @AclAncestor("pcmt_permission_drafts_approve")
+     */
     public function approveAction(Request $request): JsonResponse
     {
         throw new NotImplementedException('method not impemented');
