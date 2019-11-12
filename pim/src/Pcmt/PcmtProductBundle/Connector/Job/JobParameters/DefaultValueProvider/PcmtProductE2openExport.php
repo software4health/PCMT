d81c7632701d1c9ad4695ebe6f53a6c35b25f5d1
Krzysztof <kgrabowski@soldevelo.com>
2019-11-12 16:39:47 +0100

    #66 Content tab with correct filters

---
+++ pim/src/Pcmt/PcmtProductBundle/Connector/Job/JobParameters/DefaultValueProvider/PcmtProductE2openExport.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Pcmt\PcmtProductBundle\Connector\Job\JobParameters\DefaultValueProvider;
+
+use Akeneo\Channel\Component\Repository\ChannelRepositoryInterface;
+use Akeneo\Channel\Component\Repository\LocaleRepositoryInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\Operators;
+use Akeneo\Tool\Component\Batch\Job\JobInterface;
+use Akeneo\Tool\Component\Batch\Job\JobParameters\DefaultValuesProviderInterface;
+use Akeneo\Tool\Component\Localization\Localizer\LocalizerInterface;
+
+/**
+ * DefaultParameters for product XLSX export
+ *
+ * @author    Marie Bochu <marie.bochu@akeneo.com>
+ * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class PcmtProductE2openExport implements DefaultValuesProviderInterface
+{
+    /** @var DefaultValuesProviderInterface */
+    protected $simpleProvider;
+
+    /** @var ChannelRepositoryInterface */
+    protected $channelRepository;
+
+    /** @var LocaleRepositoryInterface */
+    protected $localeRepository;
+
+    /** @var array */
+    protected $supportedJobNames;
+
+    /**
+     * @param DefaultValuesProviderInterface $simpleProvider
+     * @param ChannelRepositoryInterface     $channelRepository
+     * @param LocaleRepositoryInterface      $localeRepository
+     * @param array                          $supportedJobNames
+     */
+    public function __construct(
+        DefaultValuesProviderInterface $simpleProvider,
+        ChannelRepositoryInterface $channelRepository,
+        LocaleRepositoryInterface $localeRepository,
+        array $supportedJobNames
+    ) {
+        $this->simpleProvider = $simpleProvider;
+        $this->channelRepository = $channelRepository;
+        $this->localeRepository = $localeRepository;
+        $this->supportedJobNames = $supportedJobNames;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultValues()
+    {
+        $parameters = $this->simpleProvider->getDefaultValues();
+        $parameters['decimalSeparator'] = LocalizerInterface::DEFAULT_DECIMAL_SEPARATOR;
+        $parameters['dateFormat'] = LocalizerInterface::DEFAULT_DATE_FORMAT;
+        $parameters['with_media'] = true;
+        $parameters['linesPerFile'] = 10000;
+
+        $channels = $this->channelRepository->getFullChannels();
+        $defaultChannelCode = (0 !== count($channels)) ? $channels[0]->getCode() : null;
+
+        $localesCodes = $this->localeRepository->getActivatedLocaleCodes();
+        $defaultLocaleCodes = (0 !== count($localesCodes)) ? [$localesCodes[0]] : [];
+
+        $parameters['filters'] = [
+            'data'      => [
+            ],
+            'structure' => [
+                'scope'   => $defaultChannelCode,
+                'locales' => $defaultLocaleCodes,
+            ],
+        ];
+
+        return $parameters;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(JobInterface $job)
+    {
+        return in_array($job->getName(), $this->supportedJobNames);
+    }
+}
