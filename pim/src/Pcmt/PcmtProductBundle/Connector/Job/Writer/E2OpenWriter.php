6be2a10ba26c6d86ff6cb74fbf5793a15cad5f77
Krzysztof Grabowski <kgrabowski@soldevelo.com>
2019-11-19 13:01:04 +0000

    #66 Resolve "Export the product list(s) in the E2OPEN import format"

---
+++ pim/src/Pcmt/PcmtProductBundle/Connector/Job/Writer/E2OpenWriter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pcmt\PcmtProductBundle\Connector\Job\Writer;
+
+use Akeneo\Pim\Enrichment\Component\Product\Connector\Writer\File\Xlsx\ProductWriter;
+
+
+class E2OpenWriter extends ProductWriter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getPath(array $placeholders = []): string
+    {
+        $jobExecution = $this->stepExecution->getJobExecution();
+        $placeholders = array_merge(
+            $placeholders, ['%datetime%' => $jobExecution->getStartTime()->format($this->datetimeFormat)]
+        );
+        return parent::getPath($placeholders);
+    }
+}
