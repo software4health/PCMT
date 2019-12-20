b9baf8d1d61412f476ca30a72ee8a6d3df5b8eed
Damian Kryger <dkryger@soldevelo.com>
2019-12-20 07:56:12 +0000

    #198 - Draft bundle created

---
+++ pim/src/PcmtDraftBundle/PcmtDraftBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright (c) 2019, VillageReach
+ * Licensed under the Non-Profit Open Software License version 3.0.
+ * SPDX-License-Identifier: NPOSL-3.0
+ */
+
+declare(strict_types=1);
+
+namespace PcmtDraftBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PcmtDraftBundle extends Bundle
+{
+}
