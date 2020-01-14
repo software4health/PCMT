4c656a044078be05e3304575e83d0b5e7f331bc1
Josh Zamor <jzamor@gmail.com>
2020-01-14 10:50:03 -0800

    #132, add license headers
    Also this will help test the CI jobs by triggering them.

--- deploy/cron/entrypoint.sh
+++ deploy/cron/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+######################################################################
+# Copyright (c) 2019, VillageReach
+# Licensed under the Non-Profit Open Software License version 3.0.
+# SPDX-License-Identifier: NPOSL-3.0
+######################################################################
 
 if [ -z "$CRON_SCHEDULE" ]; then
     echo "Cron schedule unset..."
