99c53b242ca2fe3f4c9a96cee6dcef9aa3c34450
Piotr Borek <pborek@soldevelo.com>
2020-01-15 08:11:48 +0000

    #220 Resolve "Dashboard – the “Review” button doesn’t go anywhere/do anything"

--- pim/src/PcmtDraftBundle/Entity/ProductDraftORMRepository.php
+++ pim/src/PcmtDraftBundle/Entity/ProductDraftORMRepository.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 
 namespace PcmtDraftBundle\Entity;
 
-use Akeneo\UserManagement\Component\Model\UserInterface;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Intl\Exception\NotImplementedException;
 
@@ -18,19 +17,5 @@ class ProductDraftORMRepository extends EntityRepository implements DraftReposit
     public function findById(): AbstractDraft
     {
         throw new NotImplementedException('method not implemented');
-    }
-
-    public function getUserDrafts(UserInterface $user): array
-    {
-        $qb = $this->createQueryBuilder('d');
-        $qb->where(
-            $qb->expr()->eq('d.author', ':author')
-        );
-
-        $qb->setParameters([
-            ':author' => $user,
-        ]);
-
-        return $qb->getQuery()->getResult();
     }
 }
