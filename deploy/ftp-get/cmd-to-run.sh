e669c390f168188e514e8eb63b0d10a3bcf85176
Josh Zamor <jzamor@gmail.com>
2020-01-21 14:17:42 -0800

    #212, fixes bug downloading multiple xml files.

--- deploy/ftp-get/cmd-to-run.sh
+++ deploy/ftp-get/cmd-to-run.sh
@@ -7,21 +7,25 @@
 
 . /set-env.sh
 umask 0000
-chown docker:docker $SFTP_LOCAL_DIR
+chown docker:docker "$SFTP_LOCAL_DIR"
 
-files=$(cat /list-files.ftpbatch | envsubst | /ftp-cmd.sh)
-files=$(echo "$files" | sed '/^sftp>/d')
+mapfile -t files < <( cat /list-files.ftpbatch \
+  | envsubst \
+  | /ftp-cmd.sh \
+  | sed '/^sftp>/d' )
+printf "Files to download: "
+printf '%s, ' "${files[@]}"
 
 (
-  echo cd $SFTP_REMOTE_DIR
+  echo cd "$SFTP_REMOTE_DIR"
   echo -mkdir "$SFTP_REMOTE_ARCHIVE_DIR"
-  for file in "$files"; do
+  for file in "${files[@]}"; do
     echo get "$file"
     echo !chown docker:docker "$file"
     echo !mv "$file" "$SFTP_LOCAL_DIR/$file"
     #echo rename "$file" "$SFTP_REMOTE_ARCHIVE_DIR/$file"
   done
-) > /get-files.ftppatch 
+) > /get-files.ftpbatch 
 
-cd "/tmp/ftp-get"
-cat /get-files.ftppatch | /ftp-cmd.sh
+cd "/tmp/ftp-get" || exit 1
+cat /get-files.ftpbatch | /ftp-cmd.sh
