dd568e095cfb88605208fbb275c8fccde8d6d92c
Piotr Borek <pborek@soldevelo.com>
2020-02-19 07:44:37 +0000

    #281 PCMT migrations

---
+++ bin/pcmt-migrate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+######################################################################
+# Copyright (c) 2020, VillageReach
+# Licensed under the Non-Profit Open Software License version 3.0.
+# SPDX-License-Identifier: NPOSL-3.0
+######################################################################
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/../ddev.sh exec fpm cp -v /srv/pim/app/config/migrations.yml /srv/pim/app/config/backup_migrations.yml
+source $DIR/../ddev.sh exec fpm cp -v /srv/pim/app/config/pcmt_migrations.yml /srv/pim/app/config/migrations.yml
+source $DIR/../ddev.sh exec fpm rm -rf var/cache
+source $DIR/../ddev.sh exec fpm bin/console --env=prod doctrine:migrations:migrate
+source $DIR/../ddev.sh exec fpm cp -v /srv/pim/app/config/backup_migrations.yml /srv/pim/app/config/migrations.yml
+source $DIR/../ddev.sh exec fpm rm -rf var/cache
