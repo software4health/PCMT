4d8dd6b024154306cdaa77e298d94b870eb28225
Damian Kryger <dkryger@soldevelo.com>
2020-02-20 13:19:52 +0000

    #269 - DraftInvalidItem class covered by tests

---
+++ pim/src/PcmtDraftBundle/Tests/Connector/InvalidItems/DraftInvalidItemTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright (c) 2020, VillageReach
+ * Licensed under the Non-Profit Open Software License version 3.0.
+ * SPDX-License-Identifier: NPOSL-3.0
+ */
+
+declare(strict_types=1);
+
+namespace PcmtDraftBundle\Tests\Connector\InvalidItems;
+
+use PcmtDraftBundle\Connector\Job\InvalidItems\DraftInvalidItem;
+use PHPUnit\Framework\TestCase;
+
+class DraftInvalidItemTest extends TestCase
+{
+    public function testDraftInvalidItem(): void
+    {
+        $item = new DraftInvalidItem(56, []);
+
+        $this->assertEquals(56, $item->getDraftId());
+        $this->assertEquals([], $item->getInvalidData());
+    }
+}
