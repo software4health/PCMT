eac0d2fe403a8cc8c88c5b19aebd64b3ca07e07e
Damian Kryger <dkryger@soldevelo.com>
2020-04-09 13:19:04 +0000

    #391 - Selenium CI/CD manual run - API tests

---
+++ bin/ci-behat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+######################################################################
+# Copyright (c) 2020, VillageReach
+# Licensed under the Non-Profit Open Software License version 3.0.
+# SPDX-License-Identifier: NPOSL-3.0
+######################################################################
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR/.."
+
+export COMPOSE_INTERACTIVE_NO_CLI=1
+export DOCKER_CLIENT_TIMEOUT=120
+export COMPOSE_HTTP_TIMEOUT=120
+
+source $DIR/ci-up.sh
+./ddev.sh exec -T fpm bin/console --env=test pim:install --force --symlink --clean
+./ddev.sh exec -T fpm /srv/pim/vendor/bin/behat --suite=api
