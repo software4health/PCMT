c60a33e6ada98fa04a22d2beb513a27e670c6fc9
Piotr Borek <pborek@soldevelo.com>
2020-04-23 10:26:15 +0000

    #446 functional tests less flaky

--- pim/src/PcmtDraftBundle/FunctionalTests/WebContext.php
+++ pim/src/PcmtDraftBundle/FunctionalTests/WebContext.php
@@ -1,14 +1,20 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * Copyright (c) 2020, VillageReach
  * Licensed under the Non-Profit Open Software License version 3.0.
  * SPDX-License-Identifier: NPOSL-3.0
  */
 
-use PcmtDraftBundle\Entity\AbstractDraft;
-use PcmtDraftBundle\DataFixtures\DraftFixtureFactory;
+namespace PcmtDraftBundle\FunctionalTests;
 
-class SeleniumDraftsContext extends SeleniumBaseContext
+use Behat\Mink\Exception\ExpectationException;
+use PcmtDraftBundle\DataFixtures\DraftFixtureFactory;
+use PcmtDraftBundle\Entity\AbstractDraft;
+
+class WebContext extends \SeleniumBaseContext
 {
     protected const DRAFT_STATUSES = [
         'New'      => AbstractDraft::STATUS_NEW,
@@ -16,8 +22,21 @@ class SeleniumDraftsContext extends SeleniumBaseContext
         'Rejected' => AbstractDraft::STATUS_REJECTED,
     ];
 
-    /** @var array[int] */
+    /** @var int[] */
     protected $draftIds = [];
+
+    /**
+     * @When I click approve on last draft
+     */
+    public function iClickApproveOnLastDraft(): void
+    {
+        $links = $this->getSession()->getPage()->findAll(
+            'css',
+            'a.AknIconButton.draft-approve'
+        );
+        $lastLink = end($links);
+        $lastLink->click();
+    }
 
     /**
      * @Given There is :num quantity of drafts with status :status
@@ -51,12 +70,12 @@ class SeleniumDraftsContext extends SeleniumBaseContext
     public function iSelectDraftCheckboxesForMassAction(int $num): void
     {
         $checkboxes = $this->getSession()->getPage()->findAll(
-            'css', 'td.AknGrid-bodyCell.input-cell.draft-checkbox-bodyCell.AknGrid-bodyCell--actions > label > input'
+            'css',
+            'td.AknGrid-bodyCell.input-cell.draft-checkbox-bodyCell.AknGrid-bodyCell--actions > label > input'
         );
 
         //always select last added drafts
         for ($i = count($checkboxes) - 1; $i >= (count($checkboxes) - $num); $i--) {
-
             $checkbox = $checkboxes[$i];
             if ($checkbox instanceof \Behat\Mink\Element\NodeElement) {
                 $checkbox->check();
@@ -71,10 +90,27 @@ class SeleniumDraftsContext extends SeleniumBaseContext
      */
     public function iShouldSeeMyDraftBecomingTheLatestVersionOfTheProduct(): void
     {
-        $this->getSession()->wait(2000);
-
         foreach ($this->draftIds as $draftIdentifier) {
             $this->assertPageContainsText($draftIdentifier);
         }
+    }
+
+    /**
+     * @Then I should see my draft becoming the latest version of the product, try :attempts times
+     */
+    public function iShouldSeeMyDraftBecomingTheLatestVersionOfTheProductManyTryTimes(int $attempts): void
+    {
+        // we make a number of attempts, as the job in background may last for some time
+        for ($i = 1; $i < $attempts; $i++) {
+            try {
+                $this->iShouldSeeMyDraftBecomingTheLatestVersionOfTheProduct();
+
+                return;
+            } catch (ExpectationException $e) {
+                $this->getSession()->reload();
+                $this->getSession()->wait(2000);
+            }
+        }
+        $this->iShouldSeeMyDraftBecomingTheLatestVersionOfTheProduct();
     }
 }
