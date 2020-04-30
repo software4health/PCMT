b596f72bf6062376d59fb3f19dd69de649e97697
Josh Zamor <jzamor@gmail.com>
2020-04-30 08:22:49 -0700

    #461, add chai pcmt network

---
+++ deploy/terraform/chai-network-useast/main.tf
@@ -0,0 +1,27 @@
+######################################################################
+# Copyright (c) 2019, VillageReach
+# Licensed under the Non-Profit Open Software License version 3.0.
+# SPDX-License-Identifier: NPOSL-3.0
+######################################################################
+
+terraform {
+  backend "s3" {
+    bucket = "chai-pcmt-terraform-states"
+    key    = "chai-network-useast.tf"
+    region = "us-east-1"
+    profile = "chai"
+  }
+}
+
+provider "aws" {
+  region = "${var.aws-region}"
+  profile = "chai"
+}
+
+module "chai-network-useast" {
+  source = "../modules/pcmt-network"
+
+  aws-region  = "${var.aws-region}"
+  tag-type    = "${var.tag-type}"
+  tag-bill-to = "${var.tag-bill-to}"
+}
---
+++ deploy/terraform/chai-network-useast/terraform.tfvars
@@ -0,0 +1,4 @@
+aws-region = "us-east-1"
+
+tag-type    = "infrastructure"
+tag-bill-to = "PCMT"
