b843daff9771a0c5b15c6a58d641de9fd342109a
Piotr Borek <pborek@soldevelo.com>
2020-05-07 07:09:18 +0000

    #471 Product import error resolved

--- pim/src/PcmtDraftBundle/Service/Draft/BaseProductCreatorForDraft.php
+++ pim/src/PcmtDraftBundle/Service/Draft/BaseProductCreatorForDraft.php
@@ -12,14 +12,27 @@ namespace PcmtDraftBundle\Service\Draft;
 
 use Akeneo\Pim\Enrichment\Component\Product\Builder\ProductBuilderInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Model\EntityWithFamilyVariantInterface;
+use Akeneo\Pim\Enrichment\Component\Product\Updater\ProductUpdater;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class BaseProductCreatorForDraft implements BaseEntityCreatorInterface
 {
+    /** @var ProductUpdater */
+    private $productUpdater;
+
+    /** @var NormalizerInterface */
+    private $standardNormalizer;
+
     /** @var ProductBuilderInterface */
     private $productBuilder;
 
-    public function __construct(ProductBuilderInterface $productBuilder)
-    {
+    public function __construct(
+        ProductUpdater $productUpdater,
+        NormalizerInterface $standardNormalizer,
+        ProductBuilderInterface $productBuilder
+    ) {
+        $this->productUpdater = $productUpdater;
+        $this->standardNormalizer = $standardNormalizer;
         $this->productBuilder = $productBuilder;
     }
 
@@ -28,6 +41,24 @@ class BaseProductCreatorForDraft implements BaseEntityCreatorInterface
      */
     public function create(EntityWithFamilyVariantInterface $product)
     {
-        return $this->productBuilder->createProduct($product->getIdentifier(), $product->getFamily()->getCode());
+        $newProduct = $this->productBuilder->createProduct($product->getIdentifier(), $product->getFamily()->getCode());
+
+        if ($product->getParent()) {
+            $data = [];
+            $newProduct->setParent($product->getParent());
+            $newProduct->setFamilyVariant($product->getFamilyVariant());
+
+            $values = $this->standardNormalizer->normalize($product->getValues(), 'standard');
+
+            $attributeSet = $product->getFamilyVariant()->getVariantAttributeSet(1);
+            $axesAttributes = $attributeSet->getAxes();
+            foreach ($axesAttributes as $attribute) {
+                $code = $attribute->getCode();
+                $data['values'][$code] = $values[$code];
+            }
+            $this->productUpdater->update($newProduct, $data);
+        }
+
+        return $newProduct;
     }
 }
