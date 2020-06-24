98da6158e0596f100e17b6aaf75a4b42f33936a2
Piotr Borek <pborek@soldevelo.com>
2020-06-24 12:51:35 +0200

    handle different family in gdsn import

--- pim/src/PcmtCoreBundle/Tests/Connector/Job/E2OpenFromXmlTaskletTest.php
+++ pim/src/PcmtCoreBundle/Tests/Connector/Job/E2OpenFromXmlTaskletTest.php
@@ -12,7 +12,7 @@ use Akeneo\Pim\Enrichment\Bundle\Doctrine\Common\Saver\ProductSaver;
 use Akeneo\Pim\Enrichment\Bundle\Elasticsearch\ProductQueryBuilderFactory;
 use Akeneo\Pim\Enrichment\Component\Product\Builder\ProductBuilder;
 use Akeneo\Pim\Enrichment\Component\Product\Builder\ProductBuilderInterface;
-use Akeneo\Pim\Enrichment\Component\Product\Model\Product;
+use Akeneo\Pim\Enrichment\Component\Product\Model\ProductInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderInterface;
 use Akeneo\Tool\Component\Batch\Job\JobParameters;
 use Akeneo\Tool\Component\Batch\Model\StepExecution;
@@ -20,7 +20,9 @@ use Akeneo\Tool\Component\Connector\Step\TaskletInterface;
 use Akeneo\Tool\Component\StorageUtils\Cursor\CursorInterface;
 use Akeneo\Tool\Component\StorageUtils\Saver\SaverInterface;
 use PcmtCoreBundle\Connector\Job\E2OpenFromXmlTasklet;
+use PcmtCoreBundle\Service\E2Open\E2OpenAttributesService;
 use PcmtCoreBundle\Service\E2Open\TradeItemXmlProcessor;
+use PcmtCoreBundle\Tests\TestDataBuilder\FamilyBuilder;
 use PHPUnit\Framework\MockObject\MockObject as Mock;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -86,7 +88,18 @@ class E2OpenFromXmlTaskletTest extends KernelTestCase
         parent::setUp();
     }
 
-    public function testProcessForANewProduct(): void
+    public function dataProcess(): array
+    {
+        $family = (new FamilyBuilder())->withCode(E2OpenAttributesService::FAMILY_CODE)->build();
+        $product = (new \PcmtCoreBundle\Tests\TestDataBuilder\ProductBuilder())->withFamily($family)->build();
+
+        return [
+            [$product],
+        ];
+    }
+
+    /** @dataProvider dataProcess */
+    public function testProcessForANewProduct(ProductInterface $product): void
     {
         $importTasklet = $this->getE2OpenFromXmlTaskletInstance();
 
@@ -96,7 +109,7 @@ class E2OpenFromXmlTaskletTest extends KernelTestCase
 
         $this->productBuilderMock->expects($this->atLeastOnce())
             ->method('createProduct')
-            ->willReturn($product = new Product());
+            ->willReturn($product);
 
         $this->tradeItemProcessorMock->expects($this->exactly(3))
             ->method('setProductToUpdate');
@@ -107,21 +120,48 @@ class E2OpenFromXmlTaskletTest extends KernelTestCase
         $importTasklet->execute();
     }
 
-    public function testProcessForAnExistingProduct(): void
+    /** @dataProvider dataProcess */
+    public function testProcessForAnExistingProduct(ProductInterface $product): void
     {
         $importTasklet = $this->getE2OpenFromXmlTaskletInstance();
-
-        $product = (new \PcmtCoreBundle\Tests\TestDataBuilder\ProductBuilder())->build();
 
         $this->productsCursorMock->expects($this->atLeastOnce())
             ->method('current')
             ->willReturn($product);
+
+        $this->tradeItemProcessorMock->expects($this->atLeastOnce())->method('processNode');
 
         $this->tradeItemProcessorMock->expects($this->exactly(3))
             ->method('setProductToUpdate');
 
         $this->productSaverMock->expects($this->atLeastOnce())
             ->method('save');
+
+        $importTasklet->execute();
+    }
+
+    public function dataProcessForDifferentFamily(): array
+    {
+        $family = (new FamilyBuilder())->withCode('xxxsss')->build();
+        $product = (new \PcmtCoreBundle\Tests\TestDataBuilder\ProductBuilder())->withFamily($family)->build();
+
+        return [
+            [$product],
+        ];
+    }
+
+    /** @dataProvider dataProcessForDifferentFamily */
+    public function testProcessForDifferentFamily(ProductInterface $product): void
+    {
+        $importTasklet = $this->getE2OpenFromXmlTaskletInstance();
+
+        $this->productsCursorMock->expects($this->atLeastOnce())
+            ->method('current')
+            ->willReturn($product);
+
+        $this->tradeItemProcessorMock->expects($this->never())->method('processNode');
+
+        $this->productSaverMock->expects($this->never())->method('save');
 
         $importTasklet->execute();
     }
