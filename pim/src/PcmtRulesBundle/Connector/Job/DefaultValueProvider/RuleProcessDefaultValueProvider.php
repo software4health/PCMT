ad428792e3d474eca1bdf71750848657e30c8f1e
Piotr Borek <pborek@soldevelo.com>
2020-09-02 06:44:37 +0000

    #578 Running a rule - frame

---
+++ pim/src/PcmtRulesBundle/Connector/Job/DefaultValueProvider/RuleProcessDefaultValueProvider.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright (c) 2020, VillageReach
+ * Licensed under the Non-Profit Open Software License version 3.0.
+ * SPDX-License-Identifier: NPOSL-3.0
+ */
+
+declare(strict_types=1);
+
+namespace PcmtRulesBundle\Connector\Job\DefaultValueProvider;
+
+use Akeneo\Tool\Component\Batch\Job\JobParameters\DefaultValuesProviderInterface;
+use PcmtCoreBundle\Connector\Job\JobParameters\SupportedJobsTrait;
+
+class RuleProcessDefaultValueProvider implements DefaultValuesProviderInterface
+{
+    use SupportedJobsTrait;
+
+    public function __construct(array $supportedJobNames)
+    {
+        $this->supportedJobNames = $supportedJobNames;
+    }
+
+    public function getDefaultValues(): array
+    {
+        return [
+            'ruleId' => null,
+        ];
+    }
+}
