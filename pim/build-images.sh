fbd330aebc3f405ed4c7fac328228896ea61a168
Damian Kryger <dkryger@soldevelo.com>
2020-09-10 10:25:40 +0200

    #618 - Build failing - babel__traverse TS error

--- pim/build-images.sh
+++ pim/build-images.sh
@@ -15,7 +15,7 @@ function cleanup {
 }
 trap cleanup EXIT
 
-AKENEO_VER="v3.2.66"
+AKENEO_VER="v3.2.70"
 
 # Path of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
