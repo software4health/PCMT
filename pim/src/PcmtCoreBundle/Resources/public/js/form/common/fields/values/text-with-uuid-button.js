55e4a403713db663379a5bd09a9869bd8020ce6f
Piotr Borek <pborek@soldevelo.com>
2020-10-28 08:46:15 +0000

    #629 generating unique id: product variant

---
+++ pim/src/PcmtCoreBundle/Resources/public/js/form/common/fields/values/text-with-uuid-button.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020, VillageReach
+ * Licensed under the Non-Profit Open Software License version 3.0.
+ * SPDX-License-Identifier: NPOSL-3.0
+ */
+/**
+ * @author    Yohan Blain <yohan.blain@akeneo.com>
+ * @author    Damien Carcel <damien.carcel@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+'use strict';
+
+define([
+    'pcmt/form/common/fields/text-with-uuid-button',
+    'pim/form/common/fields/values/values-behavior'
+], (BaseField, ValuesBehavior) => {
+    return BaseField.extend({
+        /**
+         * {@inheritdoc}
+         */
+        updateModel(value) {
+            ValuesBehavior.writeValue.call(this, BaseField, value);
+        },
+
+        /**
+         * {@inheritdoc}
+         */
+        getModelValue() {
+            return ValuesBehavior.readValue.call(this, BaseField);
+        }
+    });
+});
