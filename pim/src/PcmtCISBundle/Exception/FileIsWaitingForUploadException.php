1106377817367a842c72f58e4db9d92755f8f92d
Damian Kryger <dkryger@soldevelo.com>
2020-11-06 16:31:42 +0000

    #681 - CIS: FTP file to destination
    ecs fixes

---
+++ pim/src/PcmtCISBundle/Exception/FileIsWaitingForUploadException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright (c) 2020, VillageReach
+ * Licensed under the Non-Profit Open Software License version 3.0.
+ * SPDX-License-Identifier: NPOSL-3.0
+ */
+
+namespace PcmtCISBundle\Exception;
+
+class FileIsWaitingForUploadException extends \RuntimeException
+{
+}
