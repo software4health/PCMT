e9b0a0c559b6a6d621ca6df3a3e937148a3809f3
Josh Zamor <josh.zamor@villagereach.org>
2022-07-04 16:15:06 -0500

    #1176, add fhir mapping table migration

---
+++ pim/src/PcmtCoreBundle/upgrades/schema/Version20220704204334.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PcmtCoreBundle\Upgrade\Schema;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20220704204334 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $sql = <<<SQL
+DROP TABLE IF EXISTS `pcmt_cis_subscription`;
+
+CREATE TABLE `pcmt_fhir_attribute_mapping` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `code` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `attribute_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `mapping` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `searchunique_idx` (`code`)
+) ENGING=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+SQL;
+        $this->addSql($sql);
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
