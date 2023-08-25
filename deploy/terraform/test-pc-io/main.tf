f26300796058eb199960465f751715041871ba11
Josh Zamor <josh.zamor@villagereach.org>
2023-08-24 20:56:21 -0500

    add a test instance for akeneo v6 development

---
+++ deploy/terraform/test-pc-io/main.tf
@@ -0,0 +1,64 @@
+######################################################################
+# Copyright (c) 2019, VillageReach
+# Licensed under the Non-Profit Open Software License version 3.0.
+# SPDX-License-Identifier: NPOSL-3.0
+######################################################################
+
+terraform {
+  required_version = "~> 0.12.29"
+  
+  required_providers {
+    aws = {
+      version = "~> 3.1.0"
+    }
+  }
+  
+  backend "s3" {
+    bucket = "pcmt-terraform-states"
+    key    = "pcmt-demo.tf"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws-region
+}
+
+data "terraform_remote_state" "pcmt-network" {
+  backend = "s3"
+  config = {
+    bucket = "pcmt-terraform-states"
+    key    = "pcmt-network-useast.tf"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "pcmt-hosted-zone" {
+  backend = "s3"
+  config = {
+    bucket = "pcmt-terraform-states"
+    key    = "pcmt-productcatalog-io.tf"
+    region = "eu-west-1"
+  }
+}
+
+module "demo" {
+  source = "../modules/pcmt"
+  providers = {
+    aws.compute = aws
+    aws.network = aws
+  }
+
+  aws-region        = var.aws-region
+  ec2-key-pair      = var.ec2-key-pair
+  tag-name          = var.tag-name
+  tag-type          = var.tag-type
+  tag-bill-to       = var.tag-bill-to
+  root-volume-size  = var.root-volume-size
+  instance-type     = var.instance-type
+  app-deploy-group  = var.app-deploy-group
+  domain-name       = var.domain-name
+  subnet-id         = data.terraform_remote_state.pcmt-network.outputs.vpc-subnet-id
+  security-group-id = data.terraform_remote_state.pcmt-network.outputs.security-group-id
+  route53-zone-id   = data.terraform_remote_state.pcmt-hosted-zone.outputs.main-hosted-zone-id
+}
---
+++ deploy/terraform/test-pc-io/terraform.tfvars
@@ -0,0 +1,9 @@
+aws-region    = "us-east-2"
+instance-type = "t3a.small"
+root-volume-size = 40
+
+tag-name    = "test-pc-io"
+tag-type    = "demo"
+tag-bill-to = "PCMT"
+
+domain-name = "test.productcatalog.io"
